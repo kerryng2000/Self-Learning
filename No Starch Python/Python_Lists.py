287afa7fd698b12af4bf37579b95d78e3cfbb4cc
kerryng2000 <33183687+kerryng2000@users.noreply.github.com>
2020-10-23 21:34:33 -0700

    Add files via upload

---
+++ No Starch Python/Python_Lists.py
@@ -0,0 +1,16 @@
+names = ['bob', 'todd', 'cod'];
+print(names[0]);
+print(names[1]);
+print(names[2]);
+
+print("Hello" + names[0]);
+print("Hello" + names[1]);
+print("Hello" + names[2]);
+
+#append = add
+#pop/del = remove
+#sort() - sort
+#sorted() prints list sorted but doesn't affected original
+#reverse() - reverse of list
+#len() - length/size of list
+# the last item in a list is always index -1.
